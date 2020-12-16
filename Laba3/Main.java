69b03cc492003882a8afb4b481893d9560d6d884
dioptros <diana-713@mail.ru>
2020-12-17 02:10:35 +0300

    commit

--- Laba3/Main.java
+++ Laba3/Main.java
@@ -54,7 +54,7 @@ public class Main extends JFrame {
         Action aboutProgramAction = new AbstractAction("О программе") {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(Main.this, "Александрова Диана\n6 группа", "О программе", JOptionPane.INFORMATION_MESSAGE);
+                JOptionPane.showMessageDialog(Main.this, "Александрова Диана\n2 курс\n6 группа", "О программе", JOptionPane.INFORMATION_MESSAGE);
             }
         };
         JMenuItem aboutProgramItem = spravkaMenu.add(aboutProgramAction);
